ea988221d2a27a1ff54286abbba04c7e694a2833
Sakthi Priyan H <sakthipriyan@users.noreply.github.com>
2018-12-06 19:26:59 +0530

    Update HandsOn2.scala

--- src/main/scala/HandsOn2.scala
+++ src/main/scala/HandsOn2.scala
@@ -14,26 +14,97 @@ object HandsOn2 {
     val spark = getSpark()
     import spark.implicits._
 
-    val moviesFile = "../../../Downloads/ml-latest-small/movies.csv"
-    val tagsFile = "../../../Downloads/ml-latest-small/tags.csv"
-    val ratingsFile = "../../../Downloads/ml-latest-small/ratings.csv"
-
-    val moviesDF = loadDF(spark, moviesFile)
-    val tagsDF = loadDF(spark, tagsFile)
-    val ratingsDF = loadDF(spark, ratingsFile)
-
+    val moviesFile = "/home/crayondata.com/sakthipriyan/Downloads/ml-latest-small/movies.csv"
+    val tagsFile = "/home/crayondata.com/sakthipriyan/Downloads/ml-latest-small/tags.csv"
+    val ratingsFile = "/home/crayondata.com/sakthipriyan/Downloads/ml-latest-small/ratings.csv"
+    
     // Feature Engineering
-    // val moviesProcessedDF
-    // val tagsProcessedDF
-    // val ratingsProcessedDF
-    // val row
-    // val avgRating
-    // val avgCount
-    // val moviesLabelDF
-    // val moviesConsolidatedDF
-    // val moviesMapDF
-    // val moviesFeaturedDF
-    // moviesFeaturedDF.show(1)
+    val moviesDF = loadDF(spark, moviesFile)
+    val moviesProcessedDF = moviesDF
+      .withColumn("genres", split($"genres", "\\|"))
+      .withColumn("genresCount", size($"genres"))
+
+    val tagsDF = loadDF(spark, tagsFile)
+    val tagsProcessedDF = tagsDF
+      .groupBy("movieId").agg(collect_set("tag").as("tags"))
+      .withColumn("tagCount", size($"tags"))
+
+    val ratingsDF = loadDF(spark, ratingsFile)
+    val ratingsProcessedDF = ratingsDF
+      .groupBy($"movieId")
+      .agg(avg("rating").as("ratingAvg"),
+        count("rating").as("ratingCount"))
+    
+    val row = ratingsProcessedDF.agg(avg($"ratingAvg"), avg($"ratingCount")).first
+    val avgRating = row.getDouble(0)
+    val avgCount = row.getDouble(1)
+
+    val moviesLabelDF = ratingsProcessedDF.select($"movieId",
+      when($"ratingCount" >= avgCount && $"ratingAvg" >= avgRating, 0.0)
+        .when($"ratingCount" >= avgCount && $"ratingAvg" < avgRating, 1.0)
+        .when($"ratingCount" < avgCount && $"ratingAvg" >= avgRating, 2.0)
+        .otherwise(3.0).as("label"))
+
+    // moviesLabelDF.show()
+    val moviesConsolidatedDF = moviesProcessedDF
+      .join(tagsProcessedDF, "movieId")
+      .join(moviesLabelDF, "movieId")
+    moviesConsolidatedDF.show(2)
+
+    val moviesMergedDF = moviesConsolidatedDF
+      .withColumn("genres", arrayToMap($"genres"))
+      .withColumn("tags", arrayToMap($"tags"))
+
+    // moviesPrefinalDF.show(2)
+    val moviesFeaturedDF = moviesMergedDF.select(
+      $"movieId",
+      $"title",
+      $"label",
+      $"genresCount".as("gCount"),
+      $"tagCount".as("tCount"),
+      $"genres.Drama",
+      $"genres.Comedy",
+      $"genres.Romance",
+      $"genres.Thriller",
+      $"genres.Action",
+      $"genres.Adventure",
+      $"genres.Crime",
+      $"genres.Sci-Fi",
+      $"genres.Mystery",
+      $"genres.Fantasy",
+      $"genres.Children",
+      $"genres.Horror",
+      $"genres.Animation",
+      $"genres.Musical",
+      $"genres.War",
+      $"genres.Documentary",
+      $"genres.Film-Noir",
+      $"genres.IMAX",
+      $"genres.Western",
+      $"tags.In Netflix queue",
+      $"tags.atmospheric",
+      $"tags.superhero",
+      $"tags.Disney",
+      $"tags.religion",
+      $"tags.funny",
+      $"tags.quirky",
+      $"tags.surreal",
+      $"tags.psychology",
+      $"tags.thought-provoking",
+      $"tags.crime".as("tCrime"),
+      $"tags.suspense",
+      $"tags.politics",
+      $"tags.visually appealing",
+      $"tags.sci-fi".as("tSci-fi"),
+      $"tags.dark comedy",
+      $"tags.twist ending",
+      $"tags.dark",
+      $"tags.mental illness",
+      $"tags.comedy".as("tComedy")
+    ).na.fill(0.0)
+
+    moviesFeaturedDF.show(1)
+
     // val features
 
     // Data Transformation
